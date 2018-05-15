21c0c4bb60ff4f0bd6f0d92648146b48a06fccd2
fabs <faebeee@gmail.com>
2018-05-15 10:09:08 +0200

    Add interface declaration file

--- dist/PreAuditScriptInterface.d.ts
+++
@@ -1,5 +0,0 @@
-import { Page } from 'puppeteer';
-import LoggerInterface from './LoggerInterface';
-export default interface PreAuditScriptInterface {
-    execute(logger: LoggerInterface, page: Page): Promise<void>;
-}
---
+++ dist/index.d.ts
@@ -0,0 +1,29 @@
+import { Page } from 'puppeteer';
+export interface Budget {
+    [index: string]: string | number | boolean | undefined;
+    dreipol?: string | number | boolean;
+    performance?: string | number | boolean;
+    pwa?: string | number | boolean;
+    accessibility?: string | number | boolean;
+    'best-practices'?: string | number | boolean;
+}
+export interface DreihouseConfig {
+    url: string;
+    paths: string[];
+    folder: string;
+    report: any;
+    chromeFlags: string[];
+    saveReport: boolean;
+    disableEmulation: boolean;
+    disableThrottling: boolean;
+    budget: Budget;
+    preAuditScripts?: PreAuditScriptInterface[];
+    tag: string;
+}
+export interface LoggerInterface {
+    print(...args: string[]): void;
+    error(...args: string[]): void;
+}
+export interface PreAuditScriptInterface {
+    execute(logger: LoggerInterface, page: Page): Promise<void>;
+}
