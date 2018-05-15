5c4b6f9f2428e753d200d1fcb86a286215c17b23
fabs <faebeee@gmail.com>
2018-05-15 10:19:27 +0200

    Remove dreihouse dependency

--- dist/ConfigValidator.d.ts
+++
@@ -1,4 +0,0 @@
-import DreihouseConfig from "@dreipol/lighthouse-runner/dist/Interfaces/Config/DreihouseConfig";
-export default class ConfigValidator {
-    static validate(config: DreihouseConfig): DreihouseConfig;
-}
